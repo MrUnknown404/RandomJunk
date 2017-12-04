13927c0c5b890463aab2955f0d00b6705fc62dcf
DESKTOP-Q2MP1SP\-Unknown- <torrinmartel@gmail.com>
2017-12-03 23:40:32 -0700

    Commets/Lang

--- RandomJunk/src/main/java/init/ModItems.java
+++ RandomJunk/src/main/java/init/ModItems.java
@@ -12,24 +12,29 @@ public class ModItems {
 	
 	public static Item GunpowderApple;
 	public static Item SlimeApple;
+	
+	//Obsidian Stuff
 	public static Item ObsidianIngot;
 	
 	public static void init() {
 		//Items
 		GunpowderApple = new ItemGunpowderApple();
 		SlimeApple = new ItemSlimeApple();
+		//Obsidian Stuff
 		ObsidianIngot = new ItemObsidianIngot();
 	}
 	public static void register() {
 		//Register Items
 		GameRegistry.register(GunpowderApple);
 		GameRegistry.register(SlimeApple);
+		//Obsidian Stuff
 		GameRegistry.register(ObsidianIngot);
 	}
 	public static void registerRenders() {
 		//Register Render Items
 		registerRender(GunpowderApple);
 		registerRender(SlimeApple);
+		//Obsidian Stuff
 		registerRender(ObsidianIngot);
 	}
 	private static void registerRender(Item item) {
